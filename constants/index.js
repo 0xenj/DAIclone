fbb5653f5caa33baf600a75e21f26d3429a8f265
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-02-14 10:31:52 +0100

    change dai

--- constants/index.js
+++ constants/index.js
@@ -1,19 +1,18 @@
 module.exports = {
   FIGLET_NAME: `
-  #     # #     #  #####                                     
-#     # #     # #     # #    # ######  ####  #    #  ####  
-#     # #     # #       #    # #      #    # #   #  #      
-#     # #     # #       ###### #####  #      ####    ####  
- #   #   #   #  #       #    # #      #      #  #        # 
-  # #     # #   #     # #    # #      #    # #   #  #    # 
-   #       #     #####  #    # ######  ####  #    #  ####     
+  #####    ##   # 
+  #    #  #  #  # 
+  #    # #    # # 
+  #    # ###### # 
+  #    # #    # # 
+  #####  #    # #      
 `,
-  CONTRACT_NAME: "VVChecks",
-  TOKEN_NAME: "VV Checks",
-  TOKEN_SYMBOL: "CHECKS",
+  CONTRACT_NAME: "Dai",
+  TOKEN_NAME: "DAI",
+  TOKEN_SYMBOL: "DAI",
   CONTRACT_CONTRACT_URI: "ipfs://test123456test.test/test123contractURI.json",
   CONTRACT_BASE_URI: "ipfs://test123456test.test/",
   CONTRACT_EXTENSION_URI: ".json",
   //MAX_SUPPLY:
-  MINT_PRICE: "8 USDC",
+  //MINT_PRICE:
 };
